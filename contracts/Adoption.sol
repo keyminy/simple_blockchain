160e98d73e6adb9db63fabc2f377dc94ac2baee4
keyminy <imanaifs@gmail.com>
2023-09-08 14:57:44 +0900

    solidity part

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.7.6;
+pragma abicoder v2;
 
 import "hardhat/console.sol";
 
@@ -7,13 +8,13 @@ contract Adoption {
       //key가 map안에 존재한다면? 1이라는 값 할당
       uint256 keyExists;
       uint256 listIndex;
-      //owner address(adopter,입영자)
-      address owner;
+      string name;
+      address adopter; //입양자
    }
    //map의 key(string)은 petName이 옵니다
    mapping(string => PetStruct) public petsMap;
    string[] public petsList;
-
+   address[16] public adopters;
    //pet이 얼마나 있는지 count하는 함수
    function getPetListLength() public view returns (uint256 petCount) {
       return petsList.length;
@@ -29,33 +30,68 @@ contract Adoption {
       return false; //존재하지 않음
    }
 
+   event APetAdded(string petName);
+
    function addNewPet(string memory petName)
       public returns (bool success){
+         console.log("get in: add new pet function");
          //petExists가 아니면, we will not add anything!!
          if(!isPetExists(petName)) return false;
          //add로직
          petsMap[petName].keyExists = 1;
          petsList.push(petName);
          petsMap[petName].listIndex = petsList.length - 1;
-         // petsMap[petName].name = petName;
+         petsMap[petName].name = petName;
 
+         emit APetAdded(petName);
+         console.log("emit APetAdded: petName =", petName);
          return true;
    }
+   
+   function updatePet(string memory petName, address adopterAddress)
+      public returns (bool success){
+      
+      if(!isPetExists(petName)) return false;
 
-   address[16] public adopters;
+      petsMap[petName].adopter = adopterAddress;
+
+      emit APetAdded(petName);
+      return true;
+   }
+
+   function deletePet(string memory petName) public returns (bool success){
+      if (!isPetExists(petName)) return false;
+      //지워질 index
+      uint256 indexToDelete = petsMap[petName].listIndex;
+      string memory elementToMove = petsList[petsList.length - 1]; //petList의 마지막 원소
+      petsList[indexToDelete] = elementToMove;
+      petsMap[elementToMove].listIndex = indexToDelete;
+      petsList.pop();
+   }
+
+
 
    //adoption state거나 list의 내용이 변경될때마다 실행될 이벤트
    event APetGetAdopted(address indexed owner);
 
-   function adopt(uint256 petId) public returns (uint256) {
-        require(petId >= 0 && petId <= 15);
-        adopters[petId] = msg.sender;
+   function adopt(string memory petName) public returns (string memory) {
+      /**old */
+      //   require(petId >= 0 && petId <= 15);
+      //   adopters[petId] = msg.sender;
+      updatePet(petName,msg.sender);
 
-        emit APetGetAdopted(msg.sender);
-        return petId;
+
+      emit APetGetAdopted(msg.sender);
+      return petName;
    }
 
-   function getAdopters() public view returns (address[16] memory){
-    return adopters;
+
+
+   function getAdopters() public view returns (PetStruct[] memory){
+    PetStruct[] memory tempList = new PetStruct[](petsList.length);
+    for(uint256 i=0; i < petsList.length; i++){
+      tempList[i] = petsMap[petsList[i]];
+    }
+    return tempList;
    }
 }
